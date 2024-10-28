83e57b67a113f51b4a34911c131df7d59f015343
liujiexian <liujiexian@makeblock.com>
2024-10-28 14:30:00 +0800

    fix: base url

--- src/constants/site.ts
+++ src/constants/site.ts
@@ -1,4 +1,5 @@
 export const baseurl = 'https://socialimagesize.com';
+
 export const apihyBaseUrl = 'https://www.apihy.com';
 // export const scraperBaseUrl = 'http://45.77.124.113:8000';
 export const scraperBaseUrl = 'http://127.0.0.1:8000';
